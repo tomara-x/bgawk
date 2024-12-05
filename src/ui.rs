a62b0a2e39db9959ac1576edf7becec938439639
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-12-05 21:27:29 +0200

    unmatch

--- src/ui.rs
+++ src/ui.rs
@@ -44,116 +44,109 @@ fn egui_ui(
                 ui.selectable_value(&mut *mode, Mode::Draw, "Draw");
                 ui.selectable_value(&mut *mode, Mode::Joint, "Joint");
             });
-        match *mode {
-            Mode::Draw => {
+        if *mode == Mode::Draw {
+            ui.horizontal(|ui| {
+                ui.add(DragValue::new(&mut settings.sides).range(3..=128));
+                ui.label("sides");
+            });
+            ui.horizontal(|ui| {
+                ui.color_edit_button_rgba_unmultiplied(&mut settings.color);
+                ui.label("color");
+            });
+            egui::ComboBox::from_label("rigid body")
+                .selected_text(format!("{:?}", settings.rigid_body))
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(&mut settings.rigid_body, RigidBody::Static, "Static");
+                    ui.selectable_value(&mut settings.rigid_body, RigidBody::Dynamic, "Dynamic");
+                });
+            // TODO move to edit
+            ui.horizontal(|ui| {
+                ui.add(DragValue::new(&mut settings.collision_layer).range(0..=31));
+                ui.label("collision layer");
+            });
+        } else if *mode == Mode::Edit {
+            ui.horizontal(|ui| {
+                ui.label("gravity");
+                ui.add(DragValue::new(&mut gravity.0.x));
+                ui.add(DragValue::new(&mut gravity.0.y));
+            });
+            ui.add(
+                TextEdit::multiline(&mut update_code.0)
+                    .hint_text("code here will be quietly evaluated every frame")
+                    .font(TextStyle::Monospace)
+                    .code_editor()
+                    .lock_focus(true)
+                    .layouter(&mut layouter),
+            );
+            lapis.quiet_eval(&update_code.0);
+            ui.label("selected:");
+            if let Ok((mut code, mut links)) = selected.get_single_mut() {
                 ui.horizontal(|ui| {
-                    ui.add(DragValue::new(&mut settings.sides).range(3..=128));
-                    ui.label("sides");
+                    ui.label("links");
+                    ui.add(
+                        TextEdit::multiline(&mut links.0)
+                            .font(TextStyle::Monospace)
+                            .code_editor()
+                            .desired_rows(1)
+                            .lock_focus(true)
+                            .layouter(&mut layouter),
+                    )
+                    .on_hover_text(
+                        "link a property of this entity to a shared var\n\n\
+                    every line should follow the form:\n\
+                    property > variable\n\
+                    to set the variable to the property's value\n\
+                    or\n\
+                    property < variable\n\
+                    to set the property to the variable's value\n\n\
+                    properties list:\n\
+                    x\n\
+                    y\n\
+                    rx (x radius)\n\
+                    ry\n\
+                    rot (rotation)\n\
+                    mass\n\
+                    vx (x velocity)\n\
+                    vy\n\
+                    va (angular velocity)\n\
+                    restitution\n\
+                    lindamp (linear damping)\n\
+                    angdamp (angular damping)\n\
+                    inertia\n\
+                    h (hue)\n\
+                    s (saturation)\n\
+                    l (lightness)\n\
+                    a (alpha)\n\
+                    sides",
+                    );
                 });
                 ui.horizontal(|ui| {
-                    ui.color_edit_button_rgba_unmultiplied(&mut settings.color);
-                    ui.label("color");
-                });
-                egui::ComboBox::from_label("rigid body")
-                    .selected_text(format!("{:?}", settings.rigid_body))
-                    .show_ui(ui, |ui| {
-                        ui.selectable_value(&mut settings.rigid_body, RigidBody::Static, "Static");
-                        ui.selectable_value(
-                            &mut settings.rigid_body,
-                            RigidBody::Dynamic,
-                            "Dynamic",
-                        );
-                    });
-                // TODO move to edit
-                ui.horizontal(|ui| {
-                    ui.add(DragValue::new(&mut settings.collision_layer).range(0..=31));
-                    ui.label("collision layer");
+                    ui.label("code");
+                    ui.add(
+                        TextEdit::multiline(&mut code.0)
+                            .font(TextStyle::Monospace)
+                            .code_editor()
+                            .desired_rows(1)
+                            .lock_focus(true)
+                            .layouter(&mut layouter),
+                    )
+                    .on_hover_text(
+                        "code that will execute on collision\n\
+                    these placeholders will be substituted:\n\
+                    $x for this entity's x position\n\
+                    $y for y position\n\
+                    $rx for x radius\n\
+                    $ry for y radius\n\
+                    $rot for rotation\n\
+                    $vx for x velocity\n\
+                    $vy for y velocity\n\
+                    $va for angular velocity\n\
+                    $mass for.. well the mass\n\
+                    $inertia for angular inertia",
+                    );
                 });
             }
-            Mode::Edit => {
-                ui.horizontal(|ui| {
-                    ui.label("gravity");
-                    ui.add(DragValue::new(&mut gravity.0.x));
-                    ui.add(DragValue::new(&mut gravity.0.y));
-                });
-                ui.add(
-                    TextEdit::multiline(&mut update_code.0)
-                        .hint_text("code here will be quietly evaluated every frame")
-                        .font(TextStyle::Monospace)
-                        .code_editor()
-                        .lock_focus(true)
-                        .layouter(&mut layouter),
-                );
-                lapis.quiet_eval(&update_code.0);
-                ui.label("selected:");
-                if let Ok((mut code, mut links)) = selected.get_single_mut() {
-                    ui.horizontal(|ui| {
-                        ui.label("links");
-                        ui.add(
-                            TextEdit::multiline(&mut links.0)
-                                .font(TextStyle::Monospace)
-                                .code_editor()
-                                .desired_rows(1)
-                                .lock_focus(true)
-                                .layouter(&mut layouter),
-                        )
-                        .on_hover_text(
-                            "link a property of this entity to a shared var\n\n\
-                        every line should follow the form:\n\
-                        property > variable\n\
-                        to set the variable to the property's value\n\
-                        or\n\
-                        property < variable\n\
-                        to set the property to the variable's value\n\n\
-                        properties list:\n\
-                        x\n\
-                        y\n\
-                        rx (x radius)\n\
-                        ry\n\
-                        rot (rotation)\n\
-                        mass\n\
-                        vx (x velocity)\n\
-                        vy\n\
-                        va (angular velocity)\n\
-                        restitution\n\
-                        lindamp (linear damping)\n\
-                        angdamp (angular damping)\n\
-                        inertia\n\
-                        h (hue)\n\
-                        s (saturation)\n\
-                        l (lightness)\n\
-                        a (alpha)\n\
-                        sides",
-                        );
-                    });
-                    ui.horizontal(|ui| {
-                        ui.label("code");
-                        ui.add(
-                            TextEdit::multiline(&mut code.0)
-                                .font(TextStyle::Monospace)
-                                .code_editor()
-                                .desired_rows(1)
-                                .lock_focus(true)
-                                .layouter(&mut layouter),
-                        )
-                        .on_hover_text(
-                            "code that will execute on collision\n\
-                        these placeholders will be substituted:\n\
-                        $x for this entity's x position\n\
-                        $y for y position\n\
-                        $rx for x radius\n\
-                        $ry for y radius\n\
-                        $rot for rotation\n\
-                        $vx for x velocity\n\
-                        $vy for y velocity\n\
-                        $va for angular velocity\n\
-                        $mass for.. well the mass\n\
-                        $inertia for angular inertia",
-                        );
-                    });
-                }
-            }
-            Mode::Joint => {}
+        } else if *mode == Mode::Joint {
         }
     });
     Window::new("lapis output")
