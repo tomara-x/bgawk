9011d626cf53b2e23fe77a4552e59aed82a5523b
tomara_x <86204514+tomara-x@users.noreply.github.com>
2025-01-30 02:20:18 +0200

    make sure end point is correct

--- src/lapis/nets.rs
+++ src/lapis/nets.rs
@@ -1561,9 +1561,9 @@ fn call_net(expr: &ExprCall, lapis: &mut Lapis) -> Option<Net> {
             let k = nth_path_ident(arg0, 0)?;
             let wave = lapis.data.wmap.get(&k)?.clone();
             let chan = eval_usize(arg1, lapis)?;
-            if chan < wave.channels() {
-                let start = eval_usize(arg2, lapis)?;
-                let end = eval_usize(arg3, lapis)?;
+            let start = eval_usize(arg2, lapis)?;
+            let end = eval_usize(arg3, lapis)?;
+            if chan < wave.channels() && end <= wave.len() {
                 let loop_point = if let Some(arg) = arg4 {
                     eval_usize(arg, lapis)
                 } else {
