b2185816723d8ec3e25957a5e780445213a1ff5e
tomara_x <414d59@gmail.com>
2025-11-01 01:36:35 +0200

    replace

--- src/lapis/strings.rs
+++ src/lapis/strings.rs
@@ -1,4 +1,4 @@
-use super::{floats::*, helpers::*, Lapis};
+use super::{floats::*, helpers::*, ints::*, Lapis};
 use syn::*;
 
 pub fn eval_string(expr: &Expr, lapis: &Lapis) -> Option<String> {
@@ -25,6 +25,19 @@ fn call_string(expr: &ExprCall, lapis: &Lapis) -> Option<String> {
         "file" => {
             let path = eval_string(expr.args.first()?, lapis)?;
             std::fs::read_to_string(path).ok()
+        }
+        "replace" => {
+            let string = eval_string(expr.args.first()?, lapis)?;
+            let from = eval_string(expr.args.get(1)?, lapis)?;
+            let to = eval_string(expr.args.get(2)?, lapis)?;
+            Some(string.replace(&from, &to))
+        }
+        "replacen" => {
+            let string = eval_string(expr.args.first()?, lapis)?;
+            let from = eval_string(expr.args.get(1)?, lapis)?;
+            let to = eval_string(expr.args.get(2)?, lapis)?;
+            let n = eval_usize(expr.args.get(3)?, lapis)?;
+            Some(string.replacen(&from, &to, n))
         }
         "format" => {
             let mut string = eval_string(expr.args.first()?, lapis)?;
